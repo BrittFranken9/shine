2798ff872964ef9be186b2a10da402ea06bff424
Britt Franken <britt.franken9@gmail.com>
2025-01-07 22:46:36 +0100

    home deel 1

--- src/pages/light_mode/home.js
+++ src/pages/light_mode/home.js
@@ -1,9 +1,20 @@
+import Link from 'next/link';
 import styles from '@/styles/light_mode/home.module.css';
 
 export default function DarkModeHome() {
     return (
-        <div className={styles.darkBackground}>
-            <h1>Welkom bij Light Mode Home</h1>
+        <div className={styles.lightBackground}>
+            <main>
+                <h1>Welkom bij Light Mode Home</h1>
+            </main>
+            <footer className={styles.footer}>
+                <Link href="/light_mode/tools" passHref>
+                    <button className={styles.button}>Tools</button>
+                </Link>
+                <Link href="/light_mode/opdrachten" passHref>
+                    <button className={styles.button}>Opdrachten</button>
+                </Link>
+            </footer>
         </div>
     );
 }
