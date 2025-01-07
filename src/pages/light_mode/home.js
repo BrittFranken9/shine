eb092c26b25b573311411c58bbc653ae2fd3686a
Britt Franken <britt.franken9@gmail.com>
2025-01-07 20:50:33 +0100

    dark and light

--- src/pages/light_mode/home.js
+++ src/pages/light_mode/home.js
@@ -1,7 +1,9 @@
-export default function LightModeHome() {
+import styles from '@/styles/light_mode/home.module.css';
+
+export default function DarkModeHome() {
     return (
-        <div>
-            <h1>Welkom bij Light Mode Home</h1>
+        <div className={styles.darkBackground}>
+            <h1>Welkom bij Dark Mode Home</h1>
         </div>
     );
 }
